cecdb6be145b7fcb236c06428f7eff1a0192b22a
dylanw422 <dylanwestdev@gmail.com>
2023-04-25 15:19:26 -0500

    remove security

--- pages/alpha.js
+++ pages/alpha.js
@@ -3,13 +3,13 @@ import { Flex, Text, Heading, Button, Image } from '@chakra-ui/react'
 import React, { useState, useEffect } from 'react'
 import Alpha from "@/components/Alpha"
 
-// {session ? session.user.guild.roles.includes('1090105757271339198') ? <Alpha /> : <Text>401: Unauthorized</Text> : <Text>You're Not Signed In</Text>}
+// {session ? session.user.guild.roles.includes('1094476893547397130') ? <Alpha /> : <Text>401: Unauthorized</Text> : <Text>You're Not Signed In. Go <a style={{textDecoration: 'underline'}} href='/'>Home</a></Text>}
 
 export default function AlphaDashboard() {
     const { data: session } = useSession()
     return (
-        <>
-           {session ? session.user.guild.roles.includes('1094476893547397130') ? <Alpha /> : <Text>401: Unauthorized</Text> : <Text>You're Not Signed In. Go <a style={{textDecoration: 'underline'}} href='/'>Home</a></Text>}
+        <> 
+            <Alpha />
         </>
     )
 }
